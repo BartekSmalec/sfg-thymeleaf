b39261a573037b8584669faddb029b52dbf4fc4d
bartek <smalecbartlomiej@gmail.com>
2023-07-23 19:25:26 +0200

    Update

--- src/main/java/com/bartek/sfgthymeleaf/controllers/ProductController.java
+++ src/main/java/com/bartek/sfgthymeleaf/controllers/ProductController.java
@@ -6,12 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductController {
 
-    private ProductService productService;
+    private final ProductService productService;
 
     public ProductController(ProductService productService) {
         this.productService = productService;
